ab9abf3b54d8e2575f6046fa14fac12454062be2
vladyslav <vlad00anonimous00@gmail.com>
2025-04-24 13:43:16 +0200

    Added a popup confirmation to archive noleggi

--- src-sportesta/src/components/NoleggioAccordion.jsx
+++ src-sportesta/src/components/NoleggioAccordion.jsx
@@ -1,9 +1,35 @@
 import React, { useState } from "react";
 import { supabase } from "../supabaseClient";
+import "../styles/noleggio.css";
 
 const AccordionItem = ({ id, item }) => {
     const [pagato, setPagato] = useState(item.pagato ? "si" : "no");
     const [loading, setLoading] = useState(false);
+
+    const [showPopup, setShowPopup] = useState(false);
+    const [dontShowAgain, setDontShowAgain] = useState(
+        localStorage.getItem("hideArchivePopup") === "true"
+    );
+
+    const handleArchiviaClick = () => {
+        if (dontShowAgain) {
+            handleArchiviazione();
+        } else {
+            setShowPopup(true);
+        }
+    };
+
+    const handleConfermaArchiviazione = () => {
+        if (dontShowAgain) {
+            localStorage.setItem("hideArchivePopup", "true");
+        }
+        setShowPopup(false);
+        handleArchiviazione();
+    };
+
+    const handleArchiviazione = () => {
+        console.log("Noleggio archiviato!");
+    };
 
     const handleUpdatePagato = async () => {
         setLoading(true);
@@ -97,13 +123,138 @@ const AccordionItem = ({ id, item }) => {
                             <option value="no">No</option>
                         </select>
                         <button
-                            className="btn btn-success btn-sm"
+                            className="btn btn-success btn-sm d-flex align-items-center"
                             onClick={handleUpdatePagato}
                             disabled={loading}
+                            style={{ gap: "10px" }}
                         >
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="16"
+                                height="16"
+                                viewBox="0 0 24 24"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeWidth="1.5"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                className="lucide lucide-rotate-cw-icon lucide-rotate-cw"
+                            >
+                                <path d="M21 12a9 9 0 1 1-9-9c2.52 0 4.93 1 6.74 2.74L21 8" />
+                                <path d="M21 3v5h-5" />
+                            </svg>
                             {loading ? "Salvando..." : "Aggiorna"}
                         </button>
                     </div>
+                    <br />
+                    <button
+                        className="btn btn-outline-secondary btn-sm d-flex align-items-center archivia"
+                        disabled={loading}
+                        onClick={handleArchiviaClick}
+                        style={{
+                            backgroundColor: "#ffcd37",
+                            color: "#fff",
+                            borderColor: "#ffcd37",
+                            gap: "10px",
+                        }}
+                    >
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            viewBox="0 0 24 24"
+                            width="16"
+                            height="16"
+                            color="#fff"
+                            fill="none"
+                        >
+                            <path
+                                d="M21 7H3V13C3 16.7712 3 18.6569 4.17157 19.8284C5.34315 21 7.22876 21 11 21H13C16.7712 21 18.6569 21 19.8284 19.8284C21 18.6569 21 16.7712 21 13V7Z"
+                                stroke="#fff"
+                                strokeWidth="1.5"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            ></path>
+                            <path
+                                d="M21 7H3L4.2 5.4C5.08328 4.22229 5.52492 3.63344 6.15836 3.31672C6.7918 3 7.52786 3 9 3H15C16.4721 3 17.2082 3 17.8416 3.31672C18.4751 3.63344 18.9167 4.22229 19.8 5.4L21 7Z"
+                                stroke="#fff"
+                                strokeWidth="1.5"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            ></path>
+                            <path
+                                d="M12 11L12 17.5M9 13.5C9.58984 12.8932 11.1597 10.5 12 10.5C12.8403 10.5 14.4102 12.8932 15 13.5"
+                                stroke="#fff"
+                                strokeWidth="1.5"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            ></path>
+                        </svg>
+                        {loading ? "Archivio in corso..." : "Archivia"}
+                    </button>
+
+                    {showPopup && (
+                        <div
+                            className="modal modal-sheet position-fixed top-0 start-0 w-100 h-100 bg-dark bg-opacity-50 d-flex align-items-center justify-content-center"
+                            tabIndex="-1"
+                            role="dialog"
+                            style={{ zIndex: 10 }}
+                        >
+                            <div className="modal-dialog">
+                                <div className="modal-content rounded-3 shadow">
+                                    <div className="modal-body p-4 text-center">
+                                        <h5 className="mb-0 bold">
+                                            Sei sicuro di voler archiviare il
+                                            noleggio?
+                                        </h5>
+                                        <br />
+                                        <p className="mb-3 text-secondary">
+                                            Una volta archiviato, devi creare un
+                                            altro noleggio per questa persona.
+                                        </p>
+                                        <br />
+                                        <div className="form-check text-start mb-3">
+                                            <input
+                                                className="form-check-input"
+                                                type="checkbox"
+                                                id="noShowAgain"
+                                                checked={dontShowAgain}
+                                                onChange={(e) =>
+                                                    setDontShowAgain(
+                                                        e.target.checked
+                                                    )
+                                                }
+                                            />
+                                            <label
+                                                className="form-check-label"
+                                                htmlFor="noShowAgain"
+                                                style={{ fontSize: "14px" }}
+                                            >
+                                                Ho capito, non mostrare più il
+                                                messaggio
+                                            </label>
+                                        </div>
+                                    </div>
+                                    <div className="modal-footer flex-nowrap p-0">
+                                        <button
+                                            type="button"
+                                            className="btn btn-lg btn-link fs-6 text-decoration-none col-6 py-3 m-0 rounded-0 border-end"
+                                            onClick={
+                                                handleConfermaArchiviazione
+                                            }
+                                        >
+                                            Si
+                                        </button>
+                                        <button
+                                            type="button"
+                                            className="btn btn-lg btn-link fs-6 text-decoration-none col-6 py-3 m-0 rounded-0"
+                                            onClick={() => setShowPopup(false)}
+                                        >
+                                            No
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
