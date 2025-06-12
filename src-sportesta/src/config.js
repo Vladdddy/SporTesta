437073f755a587459b1ca79a3135cd767de3feea
vladyslav <vlad00anonimous00@gmail.com>
2025-06-12 16:55:28 +0200

    Test

--- src-sportesta/src/config.js
+++ src-sportesta/src/config.js
@@ -2,7 +2,7 @@
 export const API_CONFIG = {
     // Use production API endpoint
     BASE_URL: "https://sportesta.onrender.com",
-    
+
     // For development, you can switch back to:
     // BASE_URL: "http://localhost:3000",
 };
