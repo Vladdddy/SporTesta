f5bbb1f1ac26df6bcdab5190ba823108eecb3f31
vladyslav <vlad00anonimous00@gmail.com>
2025-06-17 11:54:02 +0200

    Test

--- src-sportesta/src/components/Attrezzo.jsx
+++ src-sportesta/src/components/Attrezzo.jsx
@@ -252,19 +252,7 @@ const AttrezziForm = () => {
                         memberData.modalitaNoleggio === "riscatto"
                             ? parseFloat(memberData.saldoFinale || 0)
                             : null,
-                    // Add equipment details from member's dettagli (exclude fields that shouldn't be stored in DB)
-                    ...Object.keys(memberData.dettagli).reduce((acc, key) => {
-                        // Remove asterisk from field names before converting to lowercase
-                        const cleanKey = key.replace("*", "").toLowerCase();
-
-                        // Skip fields that shouldn't be stored in the database
-                        if (cleanKey === "numerodipiede") {
-                            return acc;
-                        }
-
-                        acc[cleanKey] = memberData.dettagli[key];
-                        return acc;
-                    }, {}),
+                    // Equipment details are not stored in the noleggio table
                 };
                 entries.push(dataToSend);
             } else {
